9c36961e3ed6626bbc6712234af1031e9e6beede
Hayvon <aleksandrarshavskiy@gmail.com>
2021-01-27 11:48:57 +0100

    Added connect-worker.properties, edited buildAndDeployIntegrationdomain.yml

--- resources/k8s/deployments/confluent-platform/build-connect-image.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-pushd . > /dev/null
-cd $(dirname ${BASH_SOURCE[0]})
-SCRIPT_DIR=$(pwd)
-popd > /dev/null
-
-docker build -t localhost:32773/cp-connect-emob:5.5.0 --build-arg CONNECTORS="confluentinc/kafka-connect-jdbc:5.5.0 debezium/debezium-connector-postgresql:1.3.1" -f $SCRIPT_DIR/Dockerfile.connect $SCRIPT_DIR
