1fdde2f0ce941af7da9b7dbf4128604f6d8d4e2b
nh <Nadja.Hagen@novatec-gmbh.de>
2021-01-29 11:42:36 +0100

    Added job for Deploy-Config Container

--- resources/k8s/deployments/deploy/build-deploy-image.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-pushd . > /dev/null
-cd $(dirname ${BASH_SOURCE[0]})
-SCRIPT_DIR=$(pwd)
-popd > /dev/null
-
-docker build -t localhost:32773/cp-deploy-emob:5.5.0 --build-arg VERSION_CONFLUENT=5.5.0 -f $SCRIPT_DIR/Dockerfile.deploy $SCRIPT_DIR
